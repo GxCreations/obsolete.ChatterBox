9db9d695e62c0a6753b1bdcb16052856c2ec6891
Adam Bartha <abartha@macadamian.com>
2016-02-04 18:09:14 +0200

    Shrink UI when keyboard is shown for typing an IM (ContactsView)
    Validation:
    Make sure the call buttons are accessible on different devices (windows 10 phone, windows 8/10 tab) while the keyboard is visible when composing an IM

--- ChatterBox.Client.Presentation.Shared/Views/ContactsView.xaml.cs
+++ ChatterBox.Client.Presentation.Shared/Views/ContactsView.xaml.cs
@@ -1,4 +1,7 @@
 ﻿using ChatterBox.Client.Presentation.Shared.ViewModels;
+using Windows.Foundation;
+using Windows.UI.ViewManagement;
+using Windows.UI.Xaml;
 
 namespace ChatterBox.Client.Presentation.Shared.Views
 {
@@ -8,13 +11,46 @@ namespace ChatterBox.Client.Presentation.Shared.Views
         {
             InitializeComponent();
             DataContextChanged += ContactsView_DataContextChanged;
+            InputPane inputPane = InputPane.GetForCurrentView();
+            inputPane.Showing += InputPane_Showing;
+            inputPane.Hiding += InputPane_Hiding;
         }
 
         private void ContactsView_DataContextChanged(Windows.UI.Xaml.FrameworkElement sender, Windows.UI.Xaml.DataContextChangedEventArgs args)
         {
             var viewModel = DataContext as ContactsViewModel;
-            if(viewModel==null) return;
+            if (viewModel == null) return;
             viewModel.RingtoneElement = RingtoneElement;
+        }
+
+        private void InputPane_Hiding(InputPane sender, InputPaneVisibilityEventArgs args)
+        {
+            if (Visibility != Visibility.Visible)
+                return;
+
+            if (MainGrid.RowDefinitions != null && MainGrid.RowDefinitions.Count >= 1)
+            {
+                MainGrid.RowDefinitions[0].Height = new GridLength(1, GridUnitType.Star);
+                MainGrid.InvalidateArrange();
+            }
+        }
+
+        private void InputPane_Showing(InputPane sender, InputPaneVisibilityEventArgs args)
+        {
+            if (Visibility != Visibility.Visible)
+                return;
+
+            Rect coveredArea = sender.OccludedRect;
+            var value = MainGrid.ActualHeight - coveredArea.Height;
+
+            if (MainGrid.RowDefinitions != null &&
+                MainGrid.RowDefinitions.Count >= 1 &&
+                coveredArea.Height > 0 &&
+                value > 0)
+            {
+                MainGrid.RowDefinitions[0].Height = new GridLength(value);
+                MainGrid.InvalidateArrange();
+            }
         }
     }
 }
